da672637d926a8d8f1e1d6e3dde4f276b91e3508
ch0c4 <johan.sampedro@gmail.com>
2015-11-06 17:17:17 +0100

    level 1

--- js/game/app.js
+++ js/game/app.js
@@ -10,7 +10,7 @@ var $info = document.querySelector('#info_id'),
 var $elem = null;
 var instructions = [];
 var instructionIndex = 0;
-var HEIGHT = 600, WIDTH = 720;
+var HEIGHT = 592, WIDTH = 640;
 
 var blocks = {
 
@@ -88,9 +88,12 @@ var code = {
 var pGame = {
 
 	p: null,
-	dude: null,
+	ninja: null,
 	anim: null,
 	isMoving: false,
+	grid: [],
+	flag: null,
+	nbMove: 0,
 
 	init: function() {
 		pGame.p = new Phaser.Game(
@@ -100,47 +103,138 @@ var pGame = {
 			$content, {
 				preload: pGame.preload, 
 				create: pGame.create,
-				update: pGame.update
+				update: pGame.update, 
+				render: pGame.render
 			}
 		)
 	}
 };
 
-pGame.animationStopped = function (sprite, animation) {
+pGame.is = function (x, y) {
+	if(x === 544 && y === 256) {
+		return 'flag';
+	}
 
+	var tileX = Math.floor(x / 32), tileY = Math.floor(y / 32);
+
+	var tile = pGame.level.getTile(tileX, tileY);
+	console.log(tile.index);
+	if(tile.index !== 45 && tile.index !== 24 && tile.index !== 66 && tile.index !== 44 && tile.index !== 47) {
+		return 'block';
+	}
+
+	return 'floor';
+};
+
+pGame.drawGrid = function(game) {
+	for (var i = 0; i < WIDTH; i+=32) {
+		for (var j = 0; j < HEIGHT; j+=32) {
+			var g = game.add.graphics(i, j);
+			g.lineStyle(1, 0xffffff, 1);
+			g.moveTo(0, 0);  
+    		g.lineTo(32, 0);
+
+    		g.moveTo(32, 0);  
+    		g.lineTo(32, 32);
+
+    		g.moveTo(32, 32);  
+    		g.lineTo(0, 32);
+
+    		g.moveTo(0, 32);  
+    		g.lineTo(0, 0);
+		}
+	}
+};
+
+pGame.animationStopped = function (sprite, animation) {
 	animation.stop();
 	sprite.frame = 0;
 	this.isMoving = false;
-	Game.nextInstruction();
+	this.nbMove = 1;
+
+	setTimeout(function() {
+		Game.nextInstruction();
+	}, 1000);
 
 };
 
 pGame.preload = function() {
 	
-	this.load.spritesheet('dude', 'assets/characters/1.png', 64/4, 112/7);
+	this.load.spritesheet('ninja', 'assets/characters/1.png', 64/4, 112/7);
+	this.load.spritesheet('flag', 'assets/background-elements/sprite-flag.png', 64/4, 16);
+	this.load.tilemap('map_lvl1', 'assets/tilemap/lvl1.json', null, Phaser.Tilemap.TILED_JSON);
+	this.load.image('indoor', 'assets/background-elements/indoor-tileset.png');
 
 };
 
 pGame.create = function() {
 	
-	pGame.dude = this.add.sprite(200, 200, 'dude', 0);
-	pGame.dude.scale.setTo(2, 2);
-	this.stage.backgroundColor = 0xffffff;
+	this.stage.backgroundColor = 0x000000;
 
-	pGame.dude.animations.add('stand', 0, 60);
-	pGame.dude.animations.add('mLeft', [2,6,10,14], 60);
-	pGame.dude.animations.add('mRight', [3,7,11,15], 60);
-	pGame.dude.animations.add('mUp', [1,5,9,13], 60);
-	pGame.dude.animations.add('mDown', [0,4,8,12], 60);
+	pGame.level = this.add.tilemap('map_lvl1');
+	pGame.level.addTilesetImage('lvl1', 'indoor');
 
+	pGame.layerMap = pGame.level.createLayer('indoor');
+	pGame.layerMap.scale.set(2, 2);
 
+	pGame.layerMap.resizeWorld();
+
+	pGame.ninja = this.add.sprite(64, 256, 'ninja', 0);
+	pGame.ninja.scale.setTo(2, 2);
+
+	pGame.ninja.animations.add('stand', 0, 60);
+	pGame.ninja.animations.add('mLeft', [2, 6, 10, 14], 60);
+	pGame.ninja.animations.add('mRight', [3, 7, 11, 15], 60);
+	pGame.ninja.animations.add('mUp', [1, 5, 9, 13], 60);
+	pGame.ninja.animations.add('mDown', [0, 4, 8, 12], 60);
+
+	pGame.flag = this.add.sprite(544, 256, 'flag', 0);
+	pGame.flag.scale.setTo(2,2);
+
+	pGame.flag.animations.add('animate', [0, 1, 2, 3], 60);
+	pGame.flag.animations.play('animate', 4, true);
+
+	pGame.drawGrid(this);
+	
 };
+
+pGame.update = function() {
+
+	if(pGame.anim === null) {
+		return;
+	}
+
+	if(pGame.nbMove > 32) {
+
+		return;
+	}
+
+	if(pGame.isMoving && pGame.is(pGame.ninja.x, pGame.ninja.y) === 'floor') {
+		pGame.nbMove++;
+		if(pGame.anim.name === 'mLeft') {
+			pGame.ninja.x -= 1;
+		}
+
+		if(pGame.anim.name === 'mRight') {
+			pGame.ninja.x += 1;
+		}
+
+		if(pGame.anim.name === 'mUp') {
+			pGame.ninja.y -= 1;
+		}
+
+		if(pGame.anim.name === 'mDown') {
+			pGame.ninja.y += 1;
+		}
+	}
+}
 
 pGame.moveLeft = function() {
 	while(!this.isMoving) {
 		this.isMoving = true;	
-		pGame.anim = this.dude.animations.play('mLeft', 4);
+		pGame.anim = this.ninja.animations.play('mLeft', 4);
 		pGame.anim.onComplete.add(pGame.animationStopped, this);
+
 	}
 	
 }
@@ -148,7 +242,7 @@ pGame.moveLeft = function() {
 pGame.moveRight = function() {
 	while(!this.isMoving) {
 		this.isMoving = true;	
-		pGame.anim = this.dude.animations.play('mRight', 4);
+		pGame.anim = this.ninja.animations.play('mRight', 4);
 		pGame.anim.onComplete.add(pGame.animationStopped, this);
 	}
 }
@@ -156,7 +250,7 @@ pGame.moveRight = function() {
 pGame.moveUp = function() {
 	while(!this.isMoving) {
 		this.isMoving = true;	
-		pGame.anim = this.dude.animations.play('mUp', 4);
+		pGame.anim = this.ninja.animations.play('mUp', 4);
 		pGame.anim.onComplete.add(pGame.animationStopped, this);
 	}
 }
@@ -164,34 +258,12 @@ pGame.moveUp = function() {
 pGame.moveDown = function() {
 	while(!this.isMoving) {
 		this.isMoving = true;	
-		pGame.anim = this.dude.animations.play('mDown', 4);
+		pGame.anim = this.ninja.animations.play('mDown', 4);
 		pGame.anim.onComplete.add(pGame.animationStopped, this);
 	}
 }
 
-pGame.update = function() {
-	if(pGame.anim === null) {
-		return;
-	}
-	if(pGame.isMoving) {
-
-		if(pGame.anim.name === 'mLeft') {
-			pGame.dude.x -= 1;
-		}
-
-		if(pGame.anim.name === 'mRight') {
-			pGame.dude.x += 1;
-		}
-
-		if(pGame.anim.name === 'mUp') {
-			pGame.dude.y -= 1;
-		}
-
-		if(pGame.anim.name === 'mDown') {
-			pGame.dude.y += 1;
-		}
-	}
-	
+pGame.render = function () {
 }
 
 var Game = {
@@ -199,7 +271,6 @@ var Game = {
 	run: function() {
 		blocks.init();
 		code.init();
-		WIDTH = $content.offsetWidth - 20;
 		pGame.init();
 
 		$play.addEventListener('click', Game.play, false);
@@ -211,6 +282,7 @@ var Game = {
 
 		instructions = [];
 		instructionIndex = 0;
+		pGame.ninja.position.setTo(64, 256);
 		[].forEach.call($codeChildren, function($child) {
 			instructions.push($child.children[0].innerHTML);
 		});
@@ -245,8 +317,6 @@ var Game = {
 		instructionIndex++;
 		Game.execute(instructionIndex);
 	}
-
-
-}
+};
 
 Game.run();
